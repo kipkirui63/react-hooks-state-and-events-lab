55a12cc48027e176ebb5b12817f439f8c7bf646c
kipkirui63 <enocksang8356@gmail.com>
2023-07-24 12:31:59 +0300

    completed assignmnet

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  // Step 2: Create a state variable and a setter function for dark mode state
+  const [darkMode, setDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // Step 3: Add an event handler to toggle dark mode when the button is clicked
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
+  // Step 4: Update the 'className' of the 'div' based on the dark mode state
+  const appClass = darkMode ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        {/* Step 3: Add the event handler to the dark mode button */}
+        <button onClick={toggleDarkMode}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Item({ name, category }) {
+function Item({ name, price }) {
+  // Step 2: Create a state variable and a setter function for cart state
+  const [inCart, setInCart] = useState(false);
+
+  // Step 3: Add an event handler to toggle the cart state when the button is clicked
+  const toggleCart = () => {
+    setInCart((prevInCart) => !prevInCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <span>{price}</span>
+      {/* Step 4: Update the button text and add the event handler */}
+      <button onClick={toggleCart}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
